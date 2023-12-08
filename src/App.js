e544471d09a3f7006e8933ee933ede3e6b372f0d
Saeed Saeed <saeedgamebwoy@gmail.com>
2023-12-08 14:13:34 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ImageGenerator from './Components/ImageGenerator/ImageGenerator.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <ImageGenerator/>
+      
+    </div>
+  );
+}
+
+export default App;
